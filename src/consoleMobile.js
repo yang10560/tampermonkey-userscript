3972838205ca061ba3a3b612ebe22f93ff4ce1a6
mryang <yang10560@163.com>
2023-04-07 13:54:47 +0800

    consoleMobile.js

---
+++ src/consoleMobile.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         手机浏览器console控制台加载
+// @namespace    http://tampermonkey.net/
+// @version      0.4
+// @description  能让手机浏览器的像电脑脑F12一样调试代码，查看console控制台信息，查看网页源码，js调试等。
+// @author       夜雨
+// @match        *://*
+// @license        MIT
+// @require      https://cdn.staticfile.org/eruda/3.0.0/eruda.min.js
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+
+    eruda.init();
+
+    // Your code here...
+})();
