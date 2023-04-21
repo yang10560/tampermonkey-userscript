60de27abc0f7fc41132a7bbd28002603ef7a13df
mryang <yang10560@163.com>
2023-04-21 10:12:16 +0800

    csdn

--- src/文心一言去水印.js
+++ src/文心一言去水印.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         百度文心一言去水印
 // @namespace    http://tampermonkey.net/
-// @version      1.2
-// @description  去文心一言水印、去超时弹窗、去AI画图水印
+// @version      1.3
+// @description  去文心一言水印、去超时弹窗、去AI画图水印。CSDN-C知道去水印,去提问限制
 // @author       夜雨
-// @match        https://yiyan.baidu.com/*
+// @match        *://yiyan.baidu.com/*
+// @match        *://so.csdn.net/so/search*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=baidu.com
 // @grant        none
 // @license      MIT
@@ -15,11 +16,12 @@
     'use strict';
 
     var hideWater = function () {
-        let divMask = document.querySelector('div[style^="pointer-events"]')
+        let divMask = document.querySelector('div[style^="pointer-events"]') || document.querySelector('#mask')
         if (divMask) {
             let hideMask = document.createElement("style");
             hideMask.setAttribute("id", "hideStyle")
             hideMask.innerHTML = `div[style^="pointer-events"]{height:0 !important;width:0 !important;transform: rotate(90deg);}`
+            if (location.href.indexOf("csdn.net") > -1) hideMask.innerHTML = `#mask {height:0 !important;width:0 !important;transform: rotate(90deg);}`
             let divId = divMask.getAttribute("id")
             let v = document.querySelector("#hideStyle")
             if (v) {
@@ -99,6 +101,31 @@
                 }
             })
         }
+
+        //csdn限制
+        let csdn_input = document.querySelector("#chat input[class='el-input__inner']");
+        if(csdn_input && csdn_input.hasAttribute("disabled")){
+            try{
+                console.log("----anti csdn disabled-----")
+                csdn_input.removeAttribute("disabled")
+                document.querySelector("#chat .icon-send").removeAttribute("style")
+                document.querySelector("#chat .message-input div").classList.remove("is-disabled")
+            }catch (e) {
+                console.log(e)
+            }
+
+        }
+       //clear csdn tips
+        document.querySelectorAll(".record-item.left").forEach(item=>{
+            try {
+                if(item.innerText.includes("次数已用完")){
+                    console.log("----csdn delete item----")
+                    item.remove()
+                }
+            }catch (e) {
+                console.log(e)
+            }
+        })
 
     }, 500)
 
