883ca9efb01180ef0e0a796a0aec31ba8876b561
mryang <yang10560@163.com>
2023-05-10 23:41:08 +0800

    优化

--- src/search-plus.js
+++ src/search-plus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         chatGPT tools Plus（修改版）
 // @namespace    http://tampermonkey.net/
-// @version      2.1.3
+// @version      2.1.4
 // @description  Google、必应、百度、Yandex、360搜索、谷歌镜像、搜狗、Fsou、duckduckgo侧边栏Chat搜索，即刻体验AI，无需翻墙，无需注册，无需等待！
 // @author       夜雨
 // @match      https://cn.bing.com/*
@@ -136,7 +136,7 @@
     //  GM_addStyle(GM_getResourceText("markdownCss"));
     // GM_addStyle(GM_getResourceText("highlightCss"));
 
-    let JSver = '2.1.3';
+    let JSver = '2.1.4';
 
     var darkTheme = localStorage.getItem("darkTheme")
     console.log(darkTheme)
@@ -1286,12 +1286,12 @@
             divE.classList.add("gpt-container");
             divE.innerHTML = `
     <div id="gptInputBox">
-        <input placeholder="若用不了,请更新KEY或切换线路" id="gptInput" list="suggestions" type=text><button id="button_GPT" ><svg width="15px" height="15px" focusable="false" viewBox="0 0 24 24"><path fill="#34a853" d="M10 2v2a6 6 0 0 1 6 6h2a8 8 0 0 0-8-8"></path><path fill="#ea4335" d="M10 4V2a8 8 0 0 0-8 8h2c0-3.3 2.7-6 6-6"></path><path fill="#fbbc04" d="M4 10H2a8 8 0 0 0 8 8v-2c-3.3 0-6-2.69-6-6"></path><path fill="#4285f4" d="M22 20.59l-5.69-5.69A7.96 7.96 0 0 0 18 10h-2a6 6 0 0 1-6 6v2c1.85 0 3.52-.64 4.88-1.68l5.69 5.69L22 20.59"></path></svg>搜索</button>
+        <input autocomplete="off" placeholder="若用不了,请更新KEY或切换线路" id="gptInput" list="suggestions" type=text><button id="button_GPT" ><svg width="15px" height="15px" focusable="false" viewBox="0 0 24 24"><path fill="#34a853" d="M10 2v2a6 6 0 0 1 6 6h2a8 8 0 0 0-8-8"></path><path fill="#ea4335" d="M10 4V2a8 8 0 0 0-8 8h2c0-3.3 2.7-6 6-6"></path><path fill="#fbbc04" d="M4 10H2a8 8 0 0 0 8 8v-2c-3.3 0-6-2.69-6-6"></path><path fill="#4285f4" d="M22 20.59l-5.69-5.69A7.96 7.96 0 0 0 18 10h-2a6 6 0 0 1-6 6v2c1.85 0 3.52-.64 4.88-1.68l5.69 5.69L22 20.59"></path></svg>搜索</button>
         <datalist id="suggestions">
-            <option value="苹果"></option>
+            <option value="你好"></option>
         </datalist>
     </div>
-    <div class="chatSetting"><a id="chatSetting"><svg width="15px" height="15px" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13.85 22.25h-3.7c-.74 0-1.36-.54-1.45-1.27l-.27-1.89c-.27-.14-.53-.29-.79-.46l-1.8.72c-.7.26-1.47-.03-1.81-.65L2.2 15.53c-.35-.66-.2-1.44.36-1.88l1.53-1.19c-.01-.15-.02-.3-.02-.46 0-.15.01-.31.02-.46l-1.52-1.19c-.59-.45-.74-1.26-.37-1.88l1.85-3.19c.34-.62 1.11-.9 1.79-.63l1.81.73c.26-.17.52-.32.78-.46l.27-1.91c.09-.7.71-1.25 1.44-1.25h3.7c.74 0 1.36.54 1.45 1.27l.27 1.89c.27.14.53.29.79.46l1.8-.72c.71-.26 1.48.03 1.82.65l1.84 3.18c.36.66.2 1.44-.36 1.88l-1.52 1.19c.01.15.02.3.02.46s-.01.31-.02.46l1.52 1.19c.56.45.72 1.23.37 1.86l-1.86 3.22c-.34.62-1.11.9-1.8.63l-1.8-.72c-.26.17-.52.32-.78.46l-.27 1.91c-.1.68-.72 1.22-1.46 1.22zm-3.23-2h2.76l.37-2.55.53-.22c.44-.18.88-.44 1.34-.78l.45-.34 2.38.96 1.38-2.4-2.03-1.58.07-.56c.03-.26.06-.51.06-.78s-.03-.53-.06-.78l-.07-.56 2.03-1.58-1.39-2.4-2.39.96-.45-.35c-.42-.32-.87-.58-1.33-.77l-.52-.22-.37-2.55h-2.76l-.37 2.55-.53.21c-.44.19-.88.44-1.34.79l-.45.33-2.38-.95-1.39 2.39 2.03 1.58-.07.56a7 7 0 0 0-.06.79c0 .26.02.53.06.78l.07.56-2.03 1.58 1.38 2.4 2.39-.96.45.35c.43.33.86.58 1.33.77l.53.22.38 2.55z"></path><circle cx="12" cy="12" r="3.5"></circle></svg>设置</a></div>
+    <div class="chatSetting"><a id="chatSetting"><svg fill="#909399" width="15px" height="15px" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13.85 22.25h-3.7c-.74 0-1.36-.54-1.45-1.27l-.27-1.89c-.27-.14-.53-.29-.79-.46l-1.8.72c-.7.26-1.47-.03-1.81-.65L2.2 15.53c-.35-.66-.2-1.44.36-1.88l1.53-1.19c-.01-.15-.02-.3-.02-.46 0-.15.01-.31.02-.46l-1.52-1.19c-.59-.45-.74-1.26-.37-1.88l1.85-3.19c.34-.62 1.11-.9 1.79-.63l1.81.73c.26-.17.52-.32.78-.46l.27-1.91c.09-.7.71-1.25 1.44-1.25h3.7c.74 0 1.36.54 1.45 1.27l.27 1.89c.27.14.53.29.79.46l1.8-.72c.71-.26 1.48.03 1.82.65l1.84 3.18c.36.66.2 1.44-.36 1.88l-1.52 1.19c.01.15.02.3.02.46s-.01.31-.02.46l1.52 1.19c.56.45.72 1.23.37 1.86l-1.86 3.22c-.34.62-1.11.9-1.8.63l-1.8-.72c-.26.17-.52.32-.78.46l-.27 1.91c-.1.68-.72 1.22-1.46 1.22zm-3.23-2h2.76l.37-2.55.53-.22c.44-.18.88-.44 1.34-.78l.45-.34 2.38.96 1.38-2.4-2.03-1.58.07-.56c.03-.26.06-.51.06-.78s-.03-.53-.06-.78l-.07-.56 2.03-1.58-1.39-2.4-2.39.96-.45-.35c-.42-.32-.87-.58-1.33-.77l-.52-.22-.37-2.55h-2.76l-.37 2.55-.53.21c-.44.19-.88.44-1.34.79l-.45.33-2.38-.95-1.39 2.39 2.03 1.58-.07.56a7 7 0 0 0-.06.79c0 .26.02.53.06.78l.07.56-2.03 1.58 1.38 2.4 2.39-.96.45.35c.43.33.86.58 1.33.77l.53.22.38 2.55z"></path><circle cx="12" cy="12" r="3.5" fill="#909399"></circle></svg>设置</a></div>
     <div id=gptCueBox>
     <p class="chatHide" id="gptStatus">
    <select id="modeSelect">
@@ -1300,7 +1300,7 @@
       <option value="ANZZ">ANZZ</option>
       <option value="THEBAI">THEBAI</option>
       <option value="YQCLOUD">YQCLOUD</option>
-      <option value="XIAMI">XIAMI</option>
+      <option value="XIAMI">XIAMI[挂]</option>
       <option value="BNU120">BNU120</option>
       <option value="DOG2">DOG2</option>
       <option value="PIZZA">PIZZA</option>
@@ -1366,7 +1366,7 @@
     <span class="speak" style="margin-right: 10px;text-align: right">
     <a id="speakAnser" style="cursor: pointer" href="javascript:void(0)" >
        <svg width="20" height="20" viewBox="0 0 17 16">
-          <path d="M9 16.5v-9l6 4.5-6 4.5z"></path>
+          <path d="M9 16.5v-9l6 4.5-6 4.5z" fill="#909399"></path>
           <path d="M0 0h24v24H0z" fill="none"></path>
     </svg>朗读答案</a>
     
@@ -1376,7 +1376,7 @@
     
     <a id="rawAns" style="cursor: pointer" href="javascript:void(0)" >
        <svg width="13" height="13" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="12" cy="12" r="8" stroke="black" stroke-width="4" fill="none"></circle>
+            <circle cx="12" cy="12" r="8" stroke="#909399" stroke-width="4" fill="none"></circle>
     </svg>原文切换</a>
 
 </span>`;
@@ -1435,6 +1435,39 @@
             do_it()
 
         })
+
+        //搜索建议
+        document.getElementById('gptInput').addEventListener('keyup', () => {
+            let current;
+            let word = document.getElementById('gptInput').value;
+            if(!word) return;
+            if(current){
+                current.abort();
+            }
+            console.log(word)
+            current = GM_xmlhttpRequest({
+                method: "GET",
+                url: "https://www.baidu.com/sugrec?&prod=pc&wd="+encodeURIComponent(word),
+                responseType: "text",
+                onload:(r) => {
+                    //console.log(r)
+                    if (r.status === 200) {
+                        console.log(r.response);
+                        let dataList = JSON.parse(r.response).g;
+                        const su = document.querySelector('#suggestions');
+                        su.innerHTML = '';
+                        dataList && dataList.forEach(v => {
+                            const optionElement = document.createElement('option');
+                            optionElement.value = v.q;
+                            optionElement.innerText = v.q;
+                            su.appendChild(optionElement);
+                        });
+                    }
+
+                }
+            });
+        })
+
         document.getElementById('updatePubkey').addEventListener('click', () => {
             document.getElementById("gptAnswer").innerText = "正在更新，请稍后..."
             setPubkey()
@@ -1647,38 +1680,7 @@
 
             }
 
-            let current;
-            //搜索建议
-            if (isBlur()) {
-                let word = document.getElementById('gptInput').value;
-                if(!word) return;
-                if(current){
-                    current.abort();
-                }
-                current = GM_xmlhttpRequest({
-                    method: "GET",
-                    url: "https://www.baidu.com/sugrec?&prod=pc&wd="+encodeURIComponent(word),
-                    responseType: "text",
-                    onload:(r) => {
-                        //console.log(r)
-                        if (r.status === 200) {
-                            console.log(r.response);
-                            let dataList = JSON.parse(r.response).g;
-                            const su = document.querySelector('#suggestions');
-                            su.innerHTML = '';
-                            dataList && dataList.forEach(v => {
-                                const optionElement = document.createElement('option');
-                                optionElement.value = v.q;
-                                optionElement.innerText = v.q;
-                                su.appendChild(optionElement);
-                            });
-                        }
 
-                    }
-                });
-            } else {
-                console.log("搜索建议:失焦不执行")
-            }
         }
 
     }
