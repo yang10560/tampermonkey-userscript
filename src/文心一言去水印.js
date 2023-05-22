1ec196be8f9c48a66b98aed187f9ec4fbd8806b4
mryang <yang10560@163.com>
2023-05-22 18:15:46 +0800

    fix:底部空白

--- src/文心一言去水印.js
+++ src/文心一言去水印.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         百度文心一言去水印
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  去文心一言水印、去星火水印。去天工水印。去通义千问水印、去超时弹窗、去AI画图水印。CSDN-C知道去水印,去提问限制。
 // @author       夜雨
 // @match        *://yiyan.baidu.com/*
@@ -12,6 +12,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=baidu.com
 // @grant        none
 // @license      MIT
+// @runat        document-end
 // ==/UserScript==
 
 
@@ -23,27 +24,28 @@
             || document.querySelector('#mask') || document.querySelector("#watermark-wrapper")
             || document.querySelector("div[style*='pointer-events']")
 
-        if (location.href.indexOf("tongyi") > -1){
+        if (location.href.includes("tongyi")){
             divMask =  document.querySelector(".digit_watermark")
         }
 
         if (divMask) {
             let hideMask = document.createElement("style");
             hideMask.setAttribute("id", "hideStyle")
-            hideMask.innerHTML = `div[style^="pointer-events"]{height:0 !important;width:0 !important;transform: rotate(90deg);}`
-            if (location.href.indexOf("csdn.net") > -1) hideMask.innerHTML = `#mask {height:0 !important;width:0 !important;transform: rotate(90deg);}`
-            if (location.href.indexOf("xinghuo") > -1) hideMask.innerHTML = `#watermark-wrapper div {height:0 !important;width:0 !important;transform: rotate(90deg);}`
-            if (location.href.indexOf("tiangong") > -1) hideMask.innerHTML = `div[style*='pointer-events'] {height:0 !important;width:0 !important;transform: rotate(90deg);}`
-            if (location.href.indexOf("tongyi") > -1) hideMask.innerHTML = `.digit_watermark,div[style^="pointer-events"] {height:0 !important;width:0 !important;transform: rotate(90deg);}`
+            hideMask.innerHTML = `div[style^="pointer-events"]{height:0 !important;width:0 !important;transform: rotate(90deg);overflow: hidden;}`//overflow: hidden; or transform-origin: top left;
+            if (location.href.includes("csdn.net")) hideMask.innerHTML = `#mask {height:0 !important;width:0 !important;transform: rotate(90deg);overflow: hidden;}`
+            if (location.href.includes("xinghuo")) hideMask.innerHTML = `#watermark-wrapper div {height:0 !important;width:0 !important;transform: rotate(90deg);overflow: hidden;}`
+            if (location.href.includes("tiangong")) hideMask.innerHTML = `div[style*='pointer-events'] {height:0 !important;width:0 !important;transform: rotate(90deg);overflow: hidden;}`
+            if (location.href.includes("tongyi")) hideMask.innerHTML = `.digit_watermark,div[style^="pointer-events"] {height:0 !important;width:0 !important;transform: rotate(90deg);overflow: hidden;}`
+
 
             let divId = divMask.getAttribute("id")
             let v = document.querySelector("#hideStyle")
             if (v) {
                 v.remove()
-                document.body.append(hideMask)
+                document.head.append(hideMask)
             } else {
 
-                document.body.append(hideMask)
+                document.head.append(hideMask)
             }
 
         }
@@ -96,53 +98,56 @@
     //去超时弹窗。去AI画图水印
     setInterval(() => {
         document.body.parentElement.scrollTo(0, 0)
-        if (document.querySelector(".ant-modal-root")) {
-            document.querySelectorAll(".ant-modal-root").forEach(item => {
-                item.remove()
-            })
-        }
-
-        if (document.querySelector("img")) {
-            document.querySelectorAll("img").forEach(item => {
-                try {
-                    let imgUrl = item.getAttribute("src")
-                    if (imgUrl.indexOf("wm_ai") > -1) {
-                        item.setAttribute("src", imgUrl.replace(/style\/wm_ai/, ""))
-                    }
-
-                } catch (e) {
-                    //TODO handle the exception
-                }
-            })
-        }
-
-        //csdn限制
-        let csdn_input = document.querySelector("#chat input[class='el-input__inner']");
-        if(csdn_input && csdn_input.hasAttribute("disabled")){
-            try{
-                console.log("----anti csdn disabled-----")
-                csdn_input.removeAttribute("disabled")
-                document.querySelector("#chat .icon-send").removeAttribute("style")
-                document.querySelector("#chat .message-input div").classList.remove("is-disabled")
-            }catch (e) {
-                console.log(e)
-            }
-
-        }
-       //clear csdn tips
-        document.querySelectorAll(".record-item.left").forEach(item=>{
-            try {
-                if(item.innerText.includes("次数已用完")){
-                    console.log("----csdn delete item----")
+        if(location.href.includes("yiyan")){
+            if (document.querySelector(".ant-modal-root")) {
+                document.querySelectorAll(".ant-modal-root").forEach(item => {
                     item.remove()
-                }
-            }catch (e) {
-                console.log(e)
+                })
             }
-        })
 
-    }, 500)
+            if (document.querySelector("img")) {
+                document.querySelectorAll("img").forEach(item => {
+                    try {
+                        let imgUrl = item.getAttribute("src")
+                        if (imgUrl.indexOf("wm_ai") > -1) {
+                            item.setAttribute("src", imgUrl.replace(/style\/wm_ai/, ""))
+                        }
+
+                    } catch (e) {
+                        //TODO handle the exception
+                    }
+                })
+            }
+        }
+
+        if(location.href.indexOf("csdn")){
+            //csdn限制
+            let csdn_input = document.querySelector("#chat input[class='el-input__inner']");
+            if(csdn_input && csdn_input.hasAttribute("disabled")){
+                try{
+                    console.log("----anti csdn disabled-----")
+                    csdn_input.removeAttribute("disabled")
+                    document.querySelector("#chat .icon-send").removeAttribute("style")
+                    document.querySelector("#chat .message-input div").classList.remove("is-disabled")
+                }catch (e) {
+                    console.log(e)
+                }
+
+            }
+            //clear csdn tips
+            document.querySelectorAll(".record-item.left").forEach(item=>{
+                try {
+                    if(item.innerText.includes("次数已用完")){
+                        console.log("----csdn delete item----")
+                        item.remove()
+                    }
+                }catch (e) {
+                    console.log(e)
+                }
+            })
+        }
+
+    }, 1000)
 
 
-    // Your code here...
 })();
