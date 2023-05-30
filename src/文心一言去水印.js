dcd4aca79beb0086bdb90aa1715d360906863a1c
mryang <yang10560@163.com>
2023-05-30 08:23:08 +0800

    fix:csdn

--- src/文心一言去水印.js
+++ src/文心一言去水印.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         百度文心一言去水印
 // @namespace    http://tampermonkey.net/
-// @version      1.9
+// @version      2.0
 // @description  去文心一言水印、去星火水印。去天工水印。去chatGLM水印。去通义千问水印、去超时弹窗、去AI画图水印。CSDN-C知道去水印,去提问限制。
 // @author       夜雨
 // @match        *://yiyan.baidu.com/*
 // @match        *://so.csdn.net/so/search*
+// @match        *://so.csdn.net/so/ai*
 // @match        *://xinghuo.xfyun.cn/*
 // @match        *://neice.tiangong.cn/*
 // @match        *://tongyi.aliyun.com/*
