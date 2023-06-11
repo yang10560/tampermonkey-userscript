b6ab2556ec17e30e770b285b6b0e6f5c4cd853ed
mryang <yang10560@163.com>
2023-06-11 14:35:32 +0800

    贴吧

--- src/autoOpenInQQ.js
+++ src/autoOpenInQQ.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         QQ链接自动打开
 // @namespace    http://yeyu1024.xyz
-// @version      1.4
-// @description  PC上使用QQ、QQ邮箱点开链接，浏览器提示非QQ官方链接页面时自动打开对应的链接。
+// @version      1.5
+// @description  PC上使用QQ、QQ邮箱点开链接，浏览器提示非QQ官方链接页面时自动打开对应的链接。另外支持贴吧
 // @author       夜雨
 // @match        *://c.pc.qq.com/*
 // @match        *://mail.qq.com/cgi-bin/*
+// @match        *://jump.bdimg.com/safecheck/*
 // @icon         https://mat1.gtimg.com/www/icon/favicon2.ico
 // @grant        none
 // @license      MIT
@@ -29,6 +30,10 @@
         if(!linkUrl){
             linkUrl = document.querySelector("div.safety-url").innerText;
         }
+    }
+
+    if(location.href.includes('jump.bdimg.com')){
+        document.querySelector("a.btn.btn-next").click()
     }
 
     if(location.href.includes('mail.qq.com')){
