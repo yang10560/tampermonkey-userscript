b1364f3ba4b4e28bdeb34ae861eb9a43d3bc4f32
mryang <yang10560@163.com>
2023-06-22 17:38:12 +0800

    fix 选词翻译不重新渲染页面

--- src/SimpleTranslate.js
+++ src/SimpleTranslate.js
@@ -2,7 +2,7 @@
 // @name         网页中英双显互译
 // @name:en      Translation between Chinese and English
 // @namespace    http://yeyu1024.xyz
-// @version      1.1.4
+// @version      1.1.5
 // @description  中英互转，双语显示。为用户提供了快速准确的中英文翻译服务。无论是在工作中处理文件、学习外语、还是在日常生活中与国际友人交流，这个脚本都能够帮助用户轻松应对语言障碍。通过简单的操作，用户只需点击就会立即把网页翻译，节省了用户手动查词或使用在线翻译工具的时间，提高工作效率。
 // @description:en Translation between Chinese and English on web pages.
 // @author       夜雨
@@ -250,7 +250,7 @@
         `))
         const old_isDoubleShow = isDoubleShow;
         isDoubleShow = false;
-        translateTo(selectTolang, document.getElementById("qs_searchBoxOuter"))
+        translateTo(selectTolang, document.getElementById("qs_searchBoxOuter"),true)
         setTimeout(()=>{
             isDoubleShow = old_isDoubleShow;
         },2000)
@@ -715,8 +715,10 @@
     }
 
     //翻译
-    async function translateTo(lang, rootNode) {
-        clearSpan(lang)
+    async function translateTo(lang, rootNode, noclear) {
+        if(!noclear){
+            clearSpan(lang)
+        }
         await auth()
         console.log(`translate to....${lang}`)
         const root = document.body;
