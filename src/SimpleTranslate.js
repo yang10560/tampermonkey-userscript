fa857cad67f0f2ac56212aba0dbdc0b46bf0c63f
mryang <yang10560@163.com>
2023-07-08 22:32:06 +0800

    排除中文网

--- src/SimpleTranslate.js
+++ src/SimpleTranslate.js
@@ -2,7 +2,7 @@
 // @name         网页中英双显互译
 // @name:en      Translation between Chinese and English
 // @namespace    http://yeyu1024.xyz
-// @version      1.4.7
+// @version      1.4.8
 // @description  中英互转，双语显示。为用户提供了快速准确的中英文翻译服务。无论是在工作中处理文件、学习外语、还是在日常生活中与国际友人交流，这个脚本都能够帮助用户轻松应对语言障碍。通过简单的操作，用户只需点击就会立即把网页翻译，节省了用户手动查词或使用在线翻译工具的时间，提高工作效率。
 // @description:en Translation between Chinese and English on web pages.
 // @author       夜雨
@@ -10,6 +10,8 @@
 // @exclude      *://*.baidu.com/*
 // @exclude      *://localhost:*/*
 // @exclude      *://127.0.0.1:*/*
+// @exclude      *://*.cn/*
+// @exclude      *://cn.bing.com/*
 // @run-at       document-end
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=translate.google.com
 // @require      https://cdn.staticfile.org/jquery/3.4.0/jquery.min.js
