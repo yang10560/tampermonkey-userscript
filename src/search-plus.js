f24f111c6be7b8dbdb6c1e442aef57b494e83eaf
mryang <yang10560@163.com>
2023-07-28 19:38:20 +0800

    fix bilibili高级弹幕背景

--- src/search-plus.js
+++ src/search-plus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         chatGPT tools Plus（修改版）
 // @namespace    http://tampermonkey.net/
-// @version      3.0.1
+// @version      3.0.2
 // @description  Google、必应、百度、Yandex、360搜索、谷歌镜像、搜狗、b站、F搜、duckduckgo、CSDN侧边栏Chat搜索，集成国内一言，星火，天工，通义AI，ChatGLM，360智脑。即刻体验AI，无需翻墙，无需注册，无需等待！
 // @description:zh-TW     Google、必應、百度、Yandex、360搜索、谷歌鏡像、搜狗、b站、F搜、duckduckgo、CSDN側邊欄Chat搜索，集成國內一言，星火，天工，通義AI，ChatGLM，360智腦。即刻體驗AI，無需翻墻，無需註冊，無需等待！
 // @author       夜雨
@@ -159,7 +159,7 @@
     'use strict';
 
 
-    const JSver = '3.0.1';
+    const JSver = '3.0.2';
 
 
     function getGPTMode() {
@@ -909,7 +909,7 @@
     }
     
     .bgtransparent{
-        background-color: transparent !important;;
+        background-color: transparent !important;
     }
     
     .floating-button {
@@ -928,6 +928,13 @@
       text-decoration: none;
       color: inherit;
       z-index: 9999 !important;
+    }
+    
+    /* fix bilibili高级弹幕背景问题 */
+    #bilibili-player pre {
+      background-color: transparent !important;
+       overflow-x: hidden; !important;
+       overflow-y: hidden; !important;
     }
     
     `)
