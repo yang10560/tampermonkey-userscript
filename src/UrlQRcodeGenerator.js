c192458b80b8dc54cfcd2c6006398be3c9f0f115
mryang <yang10560@163.com>
2023-11-24 11:44:10 +0800

    支持二维码解析

--- src/UrlQRcodeGenerator.js
+++ src/UrlQRcodeGenerator.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         网页URL二维码生成
 // @namespace    http://yeyu1024.xyz
-// @version      1.3
-// @description  生成当前网页的地址(url)的二维码，方便手机扫描
+// @version      1.4
+// @description  生成当前网页的地址(url)的二维码，方便手机扫描.支持二维码图片解析
 // @description:en Generate the QR code of the address of the current webpage (URL), which is convenient for mobile phone scanning
 // @author       夜雨
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=https://www.the-qrcode-generator.com
 // @require      https://cdn.bootcdn.net/ajax/libs/qrcodejs/1.0.0/qrcode.min.js
 // @require      https://cdn.staticfile.org/jquery/3.4.0/jquery.min.js
+// @require      https://cdn.jsdelivr.net/npm/jsqr/dist/jsQR.js
 // @grant        GM_registerMenuCommand
 // @homepageURL  https://greasyfork.org/zh-CN/scripts/480612
 // @supportURL   https://greasyfork.org/zh-CN/scripts/480612
@@ -49,6 +50,37 @@
         })
     }
 
+    function decodeImgQRcode(img){
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const code = jsQR(imageData.data, imageData.width, imageData.height);
+
+        if (code) {
+            console.log("解析结果",'二维码内容：' + code.data)
+            $("body").append(`<div id="QRContainer" class="qrcodeDiv" style="z-index: 9999 !important;left: 50px;top: 50px;/*height: 350px;*/position: fixed;background-color: white;border-radius: 10px" >
+                <textarea rows="5" style="margin: 20px" id="decodeResult"></textarea>
+                <button style="font-size: 14px;width: 70px; height: 30px;margin-top: 10px;margin-bottom:10px;border-radius: 6px;margin-left: 3px;" id="closeQRresultbtn">关闭</button>
+            </div>
+        </div>`);
+
+            let decodeResult = document.getElementById("decodeResult")
+            let QRContainer = document.getElementById("QRContainer")
+            decodeResult.innerHTML = code.data
+            const closeQRresultbtn = document.getElementById("closeQRresultbtn");
+            closeQRresultbtn.addEventListener("click",()=>{
+                QRContainer.remove();
+            })
+
+        } else {
+            alert('未找到二维码')
+        }
+    }
+
     GM_registerMenuCommand("生成二维码", function (event) {
         let QRContainer = document.getElementById("QRContainer")
         if(QRContainer){
@@ -56,6 +88,41 @@
         }
         urlQRCode()
     }, "qrcodeGenerate");
+
+    GM_registerMenuCommand("解析二维码", function (event) {
+        alert("请点击要解析含有二维码的图片.")
+        let contextMenuHandler = function(ev) {
+            ev.preventDefault();
+            if (ev.button === 0) {
+                console.log('解析二维码...');
+
+                // 获取所有的 img 元素
+                const images = document.querySelectorAll('img');
+
+                // 为每个 img 元素删除事件监听器
+                images.forEach(function(img) {
+                    img.removeEventListener('click', contextMenuHandler);
+                });
+
+                console.log('二维码解析---结束');
+                console.warn(ev.target)
+                decodeImgQRcode(ev.target)
+            }
+
+        };
+        console.log('二维码解析---启动');
+        // document.addEventListener('click', contextMenuHandler);
+
+        // 获取所有的 img 元素
+        const images = document.querySelectorAll('img');
+
+        // 为每个 img 元素添加点击事件监听器
+        images.forEach(function(img) {
+            img.addEventListener('click', contextMenuHandler);
+        });
+
+
+    }, "decodeQRImg");
 
 
 })();
