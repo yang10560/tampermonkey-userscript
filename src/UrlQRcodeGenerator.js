c647234d76dc8177b09503ab2c2d335966dad8fb
mryang <yang10560@163.com>
2023-11-24 16:41:22 +0800

    fixed:修复解析二维码时数据跨域(cross-origin data),改变jsqr引入

--- src/UrlQRcodeGenerator.js
+++ src/UrlQRcodeGenerator.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         网页URL二维码生成
 // @namespace    http://yeyu1024.xyz
-// @version      1.4
+// @version      1.5
 // @description  生成当前网页的地址(url)的二维码，方便手机扫描.支持二维码图片解析
 // @description:en Generate the QR code of the address of the current webpage (URL), which is convenient for mobile phone scanning
 // @author       夜雨
@@ -9,7 +9,6 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=https://www.the-qrcode-generator.com
 // @require      https://cdn.bootcdn.net/ajax/libs/qrcodejs/1.0.0/qrcode.min.js
 // @require      https://cdn.staticfile.org/jquery/3.4.0/jquery.min.js
-// @require      https://cdn.jsdelivr.net/npm/jsqr/dist/jsQR.js
 // @grant        GM_registerMenuCommand
 // @homepageURL  https://greasyfork.org/zh-CN/scripts/480612
 // @supportURL   https://greasyfork.org/zh-CN/scripts/480612
@@ -18,6 +17,20 @@
 
 (function() {
     'use strict';
+
+    function addjs() {
+        // require      https://cdn.jsdelivr.net/npm/jsqr/dist/jsQR.js
+        //spark-js
+        if(!document.getElementById("jsQRjs")){
+            $("head").append($(
+                '<script id="jsQRjs" src="https://cdn.jsdelivr.net/npm/jsqr/dist/jsQR.js"></script>'
+            ));
+        }
+
+    }
+    setTimeout(addjs)
+    setInterval(addjs,3000)
+
 
     function urlQRCode(){
         $("body").append(`<div id="QRContainer" class="qrcodeDiv" style="z-index: 9999 !important;left: 50px;top: 50px;/*height: 350px;*/position: fixed;background-color: white;border-radius: 10px" >
@@ -118,6 +131,7 @@
 
         // 为每个 img 元素添加点击事件监听器
         images.forEach(function(img) {
+            img.setAttribute("crossorigin","anonymous") //crossorigin="anonymous"
             img.addEventListener('click', contextMenuHandler);
         });
 
