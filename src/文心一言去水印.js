9edf517592ca26208bf76b92eead0198c87fe70f
mryang <yang10560@163.com>
2023-12-09 18:23:33 +0800

    修复：天工

--- src/文心一言去水印.js
+++ src/文心一言去水印.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         百度文心一言去水印
 // @namespace    http://tampermonkey.net/
-// @version      3.2
+// @version      3.3
 // @description  去文心一言水印、去星火水印。去天工水印。去混元水印。去chatGLM水印。去通义千问水印、去超时弹窗、去AI画图水印。CSDN-C知道去水印,去提问限制。
 // @author       夜雨
 // @match        *://yiyan.baidu.com/*
@@ -10,6 +10,7 @@
 // @match        *://so.csdn.net/chat*
 // @match        *://xinghuo.xfyun.cn/*
 // @match        *://neice.tiangong.cn/*
+// @match        *://chat.tiangong.cn/*
 // @match        *://tongyi.aliyun.com/*
 // @match        *://qianwen.aliyun.com/*
 // @match        *://chat.sensetime.com/*
