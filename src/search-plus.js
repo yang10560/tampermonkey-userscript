27b0d23d96fe8ab8c0901177ffcfbfc70dc8317b
mryang <yang10560@163.com>
2024-02-19 11:49:05 +0800

    优化：csdn默认隐藏

--- src/search-plus.js
+++ src/search-plus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         chatGPT tools Plus（修改版）
 // @namespace    http://tampermonkey.net/
-// @version      3.2.8
+// @version      3.2.9
 // @description  Google、必应、百度、Yandex、360搜索、谷歌镜像、搜狗、b站、F搜、duckduckgo、CSDN侧边栏Chat搜索，集成国内一言，星火，天工，混元，通义AI，ChatGLM，360智脑,miniMax。即刻体验AI，无需翻墙，无需注册，无需等待！
 // @description:en  Google, Bing, Baidu, Yandex, 360 Search, Google Mirror, Sogou, B Station, F Search, DuckDuckgo, CSDN sidebar CHAT search, integrate domestic words, star fire, sky work, righteous AI, Chatglm, 360 wisdom, 360 wisdom brain. Experience AI immediately, no need to turn over the wall, no registration, no need to wait!
 // @description:zh-TW     Google、必應、百度、Yandex、360搜索、谷歌鏡像、搜狗、b站、F搜、duckduckgo、CSDN側邊欄Chat搜索，集成國內一言，星火，天工，通義AI，ChatGLM，360智腦。即刻體驗AI，無需翻墻，無需註冊，無需等待！
@@ -167,7 +167,7 @@
     'use strict';
 
 
-    const JSver = '3.2.8';
+    const JSver = '3.2.9';
 
 
     function getGPTMode() {
@@ -754,13 +754,14 @@
     }
 
 
-    //bilibili.com
+    //csdn
     if (window.location.href.includes("blog.csdn.net")) {
         GM_add_box_style(1)
         addBothStyle()
         keyEvent()
         appendBox(10).then((res) => {
             pivElemAddEventAndValue(null)
+            document.getElementById('hideGptDiv').click()
         })
     }
 
