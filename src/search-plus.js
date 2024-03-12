15fc9cc53735cc228d71eca5befb8abb18ae28d8
mryang <yang10560@163.com>
2024-03-12 16:27:58 +0800

    修复OPENAI连续对话

--- src/search-plus.js
+++ src/search-plus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         chatGPT tools Plus（修改版）
 // @namespace    http://tampermonkey.net/
-// @version      3.3.2
+// @version      3.3.3
 // @description  Google、必应、百度、Yandex、360搜索、谷歌镜像、搜狗、b站、F搜、duckduckgo、CSDN侧边栏Chat搜索，集成国内一言，星火，天工，混元，通义AI，ChatGLM，360智脑,miniMax。即刻体验AI，无需翻墙，无需注册，无需等待！
 // @description:en  Google, Bing, Baidu, Yandex, 360 Search, Google Mirror, Sogou, B Station, F Search, DuckDuckgo, CSDN sidebar CHAT search, integrate domestic words, star fire, sky work, righteous AI, Chatglm, 360 wisdom, 360 wisdom brain. Experience AI immediately, no need to turn over the wall, no registration, no need to wait!
 // @description:zh-TW     Google、必應、百度、Yandex、360搜索、谷歌鏡像、搜狗、b站、F搜、duckduckgo、CSDN側邊欄Chat搜索，集成國內一言，星火，天工，通義AI，ChatGLM，360智腦。即刻體驗AI，無需翻墻，無需註冊，無需等待！
@@ -168,7 +168,7 @@
     'use strict';
 
 
-    const JSver = '3.3.2';
+    const JSver = '3.3.3';
 
 
     function getGPTMode() {
@@ -2918,7 +2918,7 @@
 
                    //连接openai websocket
                    let reqJson = JSON.parse(reqJsonStr)
-                   openai_conversation_id = reqJson.openai_conversation_id
+                   //openai_conversation_id = reqJson.openai_conversation_id
                    let wssurl = reqJson.wss_url
 
                    let socket = new WebSocket(wssurl);
@@ -2938,6 +2938,10 @@
                                        let responseItem = atob(revJSON.body)
                                        console.log(responseItem)
                                        showAnserAndHighlightCodeStr(JSON.parse(responseItem.slice(6)).message.content.parts[0])
+                                       if(JSON.parse(responseItem.slice(6)).conversation_id){
+                                           openai_conversation_id = JSON.parse(responseItem.slice(6)).conversation_id
+                                           openai_parent_message_id = JSON.parse(responseItem.slice(6)).message.id
+                                       }
                                    }catch (ex) {
                                        console.error(ex)
                                    }
